f4d84f0a3da7df9e74783578593efc915dd761b5
Mike <39208141+MikeG3@users.noreply.github.com>
2022-03-09 12:31:54 -0500

    Sync for Sprint 7 from laptop

--- WebService/app.py
+++ WebService/app.py
@@ -1,61 +1,105 @@
-from flask import Flask, render_template, Response, request, Blueprint, jsonify
+# Core Library modules
+from typing import Optional
+from flask import Flask, render_template, Response, request, Blueprint, jsonify, redirect
 from flask_cors import CORS
-#from camera import Video    eited out for testing
+from flask_sqlalchemy import SQLAlchemy
+from camera import Video
 from Models.shared import db, ma
 from Models.events import Events, EventsSchema
 import os.path
 from Services.s3_service import S3Service
+from Config import config
+import urllib.request
 
-app = Flask(__name__)
-CORS(app)
-basedir = os.path.abspath(os.path.dirname(__file__))
-# Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:abir1971@pet-project.cqlvbpbplnsv.us-east-2.rds.amazonaws.com/automated_pet_door'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# Init db
-db.init_app(app)
-# Init ma
-ma.init_app(app)
+db = SQLAlchemy()
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+motion_log = []
 
-@app.route('/events', methods=['POST'])
-def add_event():
-    classes = request.json['classes']
-    video = request.json['video']
-    access_granted = request.json['access_granted']
+def open_door():
+    print("opening door")
+    try:
+        urllib.request.urlopen('http://192.168.0.42:5000/open.html')
+    except:
+        print("Error opening URL to send door activation signal")
+	
 
-    new_event = Events(classes=str(classes), video=video, access_granted=access_granted)
-    db.session.add(new_event)
-    db.session.commit()
+def create_app(cfg: Optional[config.Config] = None) -> Flask:
+    if cfg is None:
+        cfg = config.Config()
+    app = Flask(__name__, template_folder="templates")
+    app.config.from_object(cfg)
 
-@app.route('/events', methods=['GET'])
-def get_events():
-    all_events = Events.query.all()
-    events_schema = EventsSchema(many=True)
-    result = events_schema.dump(all_events)
-    return jsonify(result)
+    CORS(app)
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    # Init db
+    db.init_app(app)
+    # Init ma
+    ma.init_app(app)
 
-@app.route('/video/url', methods=['POST'])
-def generate_video_url():
-    video_key = request.json['video']
-    s3_service = S3Service()
-    response = s3_service.generate_url(bucket='video-snapshots', key=video_key)
-    return jsonify({'videoUrl': response})
+    # Simple route the veryify the health of the web service
+    @app.route('/health_check')
+    def health_check():
+        return Response(status=200)
 
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+    @app.route('/')
+    def index():
+        return render_template('index.html')
 
+    @app.route('/events', methods=['POST'])
+    def add_event():
+        classes = request.json['classes']
+        video = request.json['video']
+        access_granted = request.json['access_granted']
 
-@app.route('/video')
-def video():
-    return Response(gen(Video()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+        new_event = Events(classes=str(classes), video=video, access_granted=access_granted)
+        db.session.add(new_event)
+        db.session.commit()
+        return Response(status=200)
+
+    @app.route('/events', methods=['GET'])
+    def get_events():
+        all_events = Events.query.all()
+        events_schema = EventsSchema(many=True)
+        result = events_schema.dump(all_events)
+        return jsonify(result)
+
+    @app.route('/video/url', methods=['POST'])
+    def generate_video_url():
+        video_key = request.json['video']
+        s3_service = S3Service()
+        response = s3_service.generate_url(bucket='video-snapshots', key=video_key)
+        return jsonify({'videoUrl': response})
+		
+    @app.route('/motion/<timestamp>')
+    def motion_detection(timestamp):
+        motion_log.append(timestamp)
+        print("motion detected at: " + timestamp)
+        return render_template('index.html')
+        #return (f" Motion Detected at: " + timestamp)
+
+    def gen(camera):
+        while True:
+            response = camera.get_frame()
+            frame = response["frame"]
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+    @app.route('/video')
+    def video():
+        return Response(gen(Video(cameraSource=app.config.get("CAMERA"))),
+                        mimetype='multipart/x-mixed-replace; boundary=frame')
+						
+    @app.route('/open.html')
+    def goHome():
+        return render_template('index.html')
+		
+		
+    @app.route('/submitbutton', methods=['POST'])
+    def submitbutton():
+        open_door()
+        return render_template('index.html')
+	
+    return app
 
 
 if __name__ == "__main__":
@@ -72,5 +116,8 @@ if __name__ == "__main__":
     if not os.path.exists('weight_files/yolov4-custom_bird_mail_new.weights'):
         print("Downloading weight file yolov4-custom_bird_mail_new.weights")
         s3_service.download_file(key='yolov4-custom_bird_mail_new.weights', bucket='weightfiles',
-                             filename='weight_files/yolov4-custom_bird_mail_new.weights')
-    app.run(debug=True)
+                                 filename='weight_files/yolov4-custom_bird_mail_new.weights')
+								 
+    app = create_app()
+    # app.run(host='127.0.0.1', port=8000)
+    app.run(host='192.168.0.56', port=8000)
